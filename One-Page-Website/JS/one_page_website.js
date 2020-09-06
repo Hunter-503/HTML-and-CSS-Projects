4ac97fcf27f9e7e5368a5a2d771005bdace7c002
Verified ✔️ <joel.hunter503@gmail.com>
2020-09-05 17:03:37 -0700

    One page update!
    Updated with javascript!

--- One-Page-Website/JS/one_page_website.js
+++ One-Page-Website/JS/one_page_website.js
@@ -22,12 +22,12 @@ function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
-    if (n > slides.lenght) {slideIndex = 1}
+    if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.lenght; i++) {
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.lenght; i++) {
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
